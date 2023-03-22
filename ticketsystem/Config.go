9efbd1a82229d521937e040145806d109234854b
DHO <dho@lmis.de>
2023-03-22 12:47:38 +0100

    provide default config

--- ticketsystem/Config.go
+++ ticketsystem/Config.go
@@ -2,6 +2,6 @@ package ticketsystem
 
 type Config struct {
 	Type       string            `yaml:"type"`
-	URL        string            `yaml:"url"`
-	Properties map[string]string `yaml:"properties""`
+	URL        string            `yaml:"url,omitempty"`
+	Properties map[string]string `yaml:"properties"`
 }
